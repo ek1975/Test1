2669cf3f30e6066a88608457761347d5e850a78c
andre_000 <auther@example.com>
2018-09-02 17:59:42 -0500

    This is the second commit

--- Test1/src/main/java/com/example/demo/Test1Application.java
+++ Test1/src/main/java/com/example/demo/Test1Application.java
@@ -8,6 +8,7 @@ public class Test1Application {
 
 	public static void main(String[] args) {
 		//Change comment 1
+		//Adding another comment - 2
 		SpringApplication.run(Test1Application.class, args);
 	}
 }
